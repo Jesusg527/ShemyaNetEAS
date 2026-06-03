5bcd2b5dc9e9f8793e72f404da9aa351412a7545
Jesus Garrison <jesusagarrison5.com>
2026-06-03 11:28:31 -0600

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -115,6 +115,7 @@ function App() {
 
       {/* SEARCH */}
       <div className="search-box">
+        <label className="search-label">Search Date</label>
         <input
           type="date"
           value={date}
